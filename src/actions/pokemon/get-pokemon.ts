049265aae3b12c06b8836b4f39d758bdd69a9e5b
Prophetaa <klaux_15@hotmail.com>
2018-10-19 18:58:45 +0200

    added attack dmg

--- src/actions/pokemon/get-pokemon.ts
+++ src/actions/pokemon/get-pokemon.ts
@@ -39,3 +39,6 @@ export function fetchPokemon(id1: number, id2: number) {
       });
   };
 }
+
+
+
--- src/reducers/pokemon2.ts
+++ src/reducers/pokemon2.ts
@@ -1,9 +1,12 @@
 import { POKEMON1_FETCHED } from "../actions/pokemon/get-pokemon";
+import { ATTACKED2 } from "../actions/battle/attackAction"
 
 export default function(state = {}, { type, payload }) {
   switch (type) {
     case POKEMON1_FETCHED:
       return payload;
+     case ATTACKED2:
+      return payload 
     default:
       return state;
   }
