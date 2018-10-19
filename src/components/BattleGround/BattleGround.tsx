09e468db2945b1a6a640b8b235f8198483ea8b01
Stefanwullems <stefanwullems1998@gmail.com>
2018-10-19 11:56:03 +0200

    redux added

--- src/components/BattleGround/BattleGround.tsx
+++ src/components/BattleGround/BattleGround.tsx
@@ -3,3 +3,5 @@ import * as React from "react";
 function BattleGround() {
   return <div>hi</div>;
 }
+
+export default BattleGround;
--- src/components/BattleGround/BattleGroundContainer.tsx
+++ src/components/BattleGround/BattleGroundContainer.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import BattleGround from "./BattleGround";
 
 class BattleGroundContainer extends React.Component {
   render() {
